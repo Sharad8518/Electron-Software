c68a725809039daba0cbc570c28c406633b9c086
Pavan Ahirwar <sharadkum9@gmail.com>
2022-04-22 12:28:25 +0530

    make clip

--- src/component/Home.js
+++ src/component/Home.js
@@ -7,30 +7,31 @@ import {Container,Row,Col,Button} from "react-bootstrap"
 
 function Home() {
   return (
-    <>
-      
-  
-   
-    
-     <h2 style={{textAlign:"center", marginTop:"100px",fontFamily:"DM Sans",fontWeight:"500"}}>Faster Bill Software in very low prize</h2>
-    <Container   style={{marginTop:"50px"}}>
-        <Row>
-        <Col md={6}>
-          
-          <h3 style={{textAlign:"center"}}  >Information Of Software</h3>
-          
-          <p  style={{textAlign:"center"}}   >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the</p>
-         
-          
-          </Col>
-        <Col md={6}><Image class="img-fluid" alt="Responsive image" src={imgsecond}/></Col>
-        </Row>
-       </Container>
-     
-     
-   
-</>
+    <div className='box'>
 
+     <Container>
+       <Row>
+         <Col md={6}>
+          <h1  style={{marginTop:"100px",fontWeight:"900", color:"#fff"}}>Beats Studio3</h1>
+          <h4 style={{color:"#fff"}}>Stylish OverEar Headphones</h4>
+    
+          <p style={{color:"#fff"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+        <Row>
+        <Col md={12}  ><Button style={{marginLeft:"10px" ,backgroundColor:"transparent" ,color:"#000", borderRadius:"none" ,border:"2px solid black"}}   >More Details</Button>
+        <Button  style={{ width:"130px",marginLeft:"10px" ,backgroundColor:"#fff" ,color:"#000", borderRadius:"none" ,border:"2px solid #fff"}}>Add Cart</Button>
+        </Col>
+       
+        </Row>
+
+
+         </Col>
+         <Col md={6}>
+         <Image className="img-fluid" alt="Responsive image" src={imgsecond}/>
+         </Col>
+       </Row>
+     </Container>
+
+    </div>
   )
 }
 
